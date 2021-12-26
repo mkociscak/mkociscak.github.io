cd459c3208e02eba7a1f00b933cf6363b26cb5e3
Megan Kociscak <megankociscak@gmail.com>
2021-12-25 19:34:30 -0500

    added js file

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+function validateForms() {
+
+
+
+    let canyouans = document.getElementById("canyou").value;
+
+    if (canyouans == "") {
+        alert("Please type yes or no");
+    } 
+    if (canyouans == "yes") {
+        alert("Yeeeah! This is NEAT!");
+    } 
+    if (canyouans == "no") {
+        alert("That's okay, try again if you want");
+    }
+}
+
+function validateForms() {
+
+
+
+    let doyouans = document.getElementById("doyou").value;
+
+    if (doyouans == "") {
+        alert("Please type yes or no");
+    } 
+    if (doyouans == "yes") {
+        alert("Yesssss! Thank you.");
+    } 
+    if (doyouans == "no") {
+        alert("Dang it! Well, that's fair.");
+    }
+
+
+}
